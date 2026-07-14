ed8384f0e44945dd3e8add458ab2f8cb0f157b02
bg7nzl <bg7nzl@163.com>
2026-07-14 13:47:41 +0000

    Update FT8 transmission delay logic to accommodate FT4 protocol

--- src/dialog_ft8.c
+++ src/dialog_ft8.c
@@ -1751,7 +1751,9 @@ static bool free_msg_ok_cb(void) {
     clock_gettime(CLOCK_REALTIME, &now);
     float time_since_slot_start = 0.0f;
     tx_time_slot = !get_time_slot(now, &time_since_slot_start);
-    if (time_since_slot_start < MAX_TX_START_DELAY) {
+    float max_delay = (subject_get_int(cfg.ft8_protocol.val) == FTX_PROTOCOL_FT8)
+                      ? MAX_TX_START_DELAY : MAX_TX_START_DELAY_FT4;
+    if (time_since_slot_start < max_delay) {
         tx_time_slot = !tx_time_slot;
     }
 
